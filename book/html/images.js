887a7473e7af8bb6b193616cde69033be11b1c7c
Anders Rune Jensen <arj03@protonmail.ch>
2017-10-24 21:54:51 +0200

    Add missing file

---
+++ book/html/images.js
@@ -0,0 +1,26 @@
+const nest = require('depnest')
+const { h, Set, map, forEach, when } = require('mutant')
+
+exports.needs = nest({
+  'blob.sync.url': 'first',
+  'blob.html.input': 'first'
+})
+
+exports.gives = nest('book.html.images')
+
+exports.create = (api) => {
+  return nest('book.html.images', images)
+  function images ({images, isEditing, onUpdate}) {
+    const allImages = Set([])
+    images(images => forEach(images, image => allImages.add(image)))
+
+    const fileInput = api.blob.html.input(file => {
+      onUpdate(file)
+    })
+
+    return h('section.images', {}, [
+      map(allImages, image => h('img', {src: api.blob.sync.url(image.link)})),
+      when(isEditing, [h('div', 'Add an image:'), fileInput])
+    ])
+  }
+}
