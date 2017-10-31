bd040e587da9831443f9a4542c1a318f354a7487
Anders Rune Jensen <arj03@protonmail.ch>
2017-10-31 21:49:19 +0100

    Fix create styling

--- book/html/create.js
+++ book/html/create.js
@@ -25,14 +25,14 @@ exports.create = function (api) {
     return h('Create -book',
              { classList: when(showCreate, '-expanded', '-contracted') }, [
       h('section.content', [
-        h('div.title', [h('label', 'Title'),
+        h('div.title', [h('label', 'Title:'),
                         h('input', {'ev-input': e => book.title.set(e.target.value),
                                     value: '' })]),
-        images({images: book.images, isEditing: true, onUpdate: book.images.add }),
-        h('div.authors', [h('label', 'Authors'),
+        h('div.authors', [h('label', 'Authors:'),
                           h('input', {'ev-input': e => book.authors.set(e.target.value),
                                       value: '' })]),
-        h('div.description', [h('label', 'Description'),
+        images({images: book.images, isEditing: true, onUpdate: book.images.add }),
+        h('div.description', [h('div', 'Description:'),
                               h('textarea', {'ev-input': e => book.description.set(e.target.value),
                                              value: '' }) ])
       ]),
